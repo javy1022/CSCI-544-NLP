41823f080a40bd58985c939834da0a2bed232e2c
Javy Wu <javywu1022@gmail.com>
2023-01-22 12:06:20 -0800

    checked that this file has no bug. Renamed stopwords function and some variables

--- HW1/src/HW1.py
+++ HW1/src/HW1.py
@@ -22,6 +22,13 @@ import warnings
 RANDOM_SAMPLE_SIZE = 20000
 
 
+def init_data(data_frame):
+    data_frame.dropna(inplace=True)
+    data_frame.drop_duplicates(inplace=True)
+    data_frame['star_rating'] = data_frame['star_rating'].astype('int')
+    return data_frame
+
+
 def init_spell_checker():
     sym_spell_obj = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
     dictionary_path = pkg_resources.resource_filename(
@@ -44,39 +51,6 @@ def spell_correct(text):
     return suggestions[0].term
 
 
-def remove_stopwords(review_body_string):
-    word_tokens = word_tokenize(review_body_string)
-    buffer_string = ""
-
-    for w in word_tokens:
-        if w not in stop_words:
-            w = word_lemmatization(w)
-            buffer_string = buffer_string + w + "　"
-
-    buffer_string = re.sub(' +', ' ', buffer_string).strip()
-
-    return buffer_string
-
-
-def get_adverb_lemma(word):
-    temp_bool = False
-    param_wn_synset = word + ".r.01"
-
-    # check if word's synset continas adverb option
-    for i in range(0, len(wn.synsets(word))):
-        if param_wn_synset == str((wn.synsets(word)[i])).split("\'")[1]:
-            temp_bool = True
-
-    if  temp_bool == False:
-        return word
-
-    # if yes and suggestion not empty then return suggestion, else return original word
-    suggest_lemma_list = wn.synset(param_wn_synset).lemmas()[0].pertainyms()
-    if len(suggest_lemma_list) > 0:
-        return suggest_lemma_list[0].name()
-    else:
-        return word
-
 def word_lemmatization(word):
     treebank_pos_tag = pos_tag([word])[0][1]
     universal_pos_tag = map_tag('en-ptb', 'universal', treebank_pos_tag)
@@ -94,9 +68,42 @@ def word_lemmatization(word):
     return word
 
 
+def get_adverb_lemma(word):
+    has_suggestion = False
+    param_wn_synset = word + ".r.01"
+
+    # check if word's synset contains adverb option
+    for i in range(0, len(wn.synsets(word))):
+        if param_wn_synset == str((wn.synsets(word)[i])).split("\'")[1]:
+            temp_bool = True
+
+    if not has_suggestion:
+        return word
+
+    # if yes and suggestion not empty then return suggestion, else return original word
+    suggest_lemma_list = wn.synset(param_wn_synset).lemmas()[0].pertainyms()
+    if len(suggest_lemma_list) > 0:
+        return suggest_lemma_list[0].name()
+    else:
+        return word
+
+
+def lemmatize_non_stopwords(review_body_string):
+    word_tokens = word_tokenize(review_body_string)
+    buffer_string = ""
+
+    for w in word_tokens:
+        if w not in stop_words:
+            w = word_lemmatization(w)
+            buffer_string = buffer_string + w + "　"
+
+    buffer_string = re.sub(' +', ' ', buffer_string).strip()
+    return buffer_string
+
+
 def data_cleaning(data_frame):
     # 0-50 for testing purpose
-    for i in range(0, 50):
+    for i in range(0, Testing_counter):
         review_text = class1_df['review_body'][i]
 
         # remove un-wanted html tags
@@ -116,9 +123,8 @@ def data_cleaning(data_frame):
         # lower case and strip
         review_text = review_text.lower().strip()
 
-        print(review_text)
         # remove stop words
-        review_text = remove_stopwords(review_text)
+        review_text = lemmatize_non_stopwords(review_text)
 
         data_frame.loc[i, ['review_body']] = review_text
         print(class1_df['review_body'][i] + "\n")
@@ -126,42 +132,38 @@ def data_cleaning(data_frame):
     return data_frame
 
 
-def init_data(data_frame):
-    data_frame.dropna(inplace=True)
-    data_frame.drop_duplicates(inplace=True)
-    data_frame['star_rating'] = data_frame['star_rating'].astype('int')
-    return data_frame
-
-
 if __name__ == '__main__':
-
     # init
     warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
-    df = pd.read_pickle("./data.pkl")
     sym_spell = init_spell_checker()
-    df = init_data(df).reset_index(drop=True)
     stop_words = set(stopwords.words('english'))
     wnl = WordNetLemmatizer()
 
+    # Using pkl file to save time when debugging. Use real source data.pkl after finish debugging.
+    # Save pkl copies of 3 classes for potential debug sessions
+    """
+    # program start
+    df = pd.read_pickle("./data.pkl")
+    df = init_data(df).reset_index(drop=True)
+   
     # 3-classes dataset
     class1_df = df[df['star_rating'] <= 2].sample(RANDOM_SAMPLE_SIZE).reset_index(drop=True)
     class2_df = df[df['star_rating'] == 3].sample(RANDOM_SAMPLE_SIZE)
     class3_df = df[df['star_rating'] >= 4].sample(RANDOM_SAMPLE_SIZE)
+    """
 
-    # working on class1_df to test code, handle duplicated rows that review_body only differ in whitespaces
+    # test counter to prevent from looping through the whole data frame, set to 20000 after finish debugging
+    Testing_counter = 50
+
+    # working on class1_df to test code, handle empty rows after data cleaning step
+
+    class1_df = pd.read_pickle("./class1_debug.pkl")
     class1_df = data_cleaning(class1_df)
-    print(class1_df.info(verbose="True"))
+    class1_df.dropna(inplace=True)
 
-    class1_df.drop_duplicates(inplace=True)
-    print(class1_df.info(verbose = "True"))
-
-    class1_df.drop_duplicates(inplace=True, subset="review_body")
-    print(class1_df.info(verbose="True"))
-
-
-
-
-
-
-
-
+    """
+    class2_df = data_cleaning(class2_df)
+    class2_df.dropna(inplace=True)
+    class3_df = data_cleaning(class3_df)
+    class3_df.dropna(inplace=True)
+    """
